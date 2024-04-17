14059dce201f6e7db41b95eeced14790210b0523
Júlio Fortunato <juliofortunato58@gmail.com>
2024-04-17 16:47:50 -0300

    ✨ feat: add ReadMore component

---
+++ app/[product]/_components/read-more.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+
+interface ReadMoreProps {
+  text: string;
+  maxLength?: number;
+}
+
+export default function ReadMore({ text, maxLength = 190 }: ReadMoreProps) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  function toggleExpanded() {
+    setIsExpanded(!isExpanded);
+  }
+
+  return (
+    <div>
+      {isExpanded ? (
+        <>
+          <p className="text-sm text-dusty-gray">
+            {text}{" "}
+            <button
+              className="text-sm font-bold text-primary"
+              onClick={toggleExpanded}
+            >
+              Read less
+            </button>
+          </p>
+        </>
+      ) : (
+        <div>
+          {text.length <= maxLength ? (
+            text
+          ) : (
+            <>
+              <p className="text-sm text-dusty-gray">
+                {`${text.slice(0, maxLength)}...`}{" "}
+                <button
+                  className="text-sm font-bold text-primary"
+                  onClick={toggleExpanded}
+                >
+                  Read more
+                </button>
+              </p>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
